a0890b2e56de7c1155400a25f2031f2f144051f9
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-12-18 12:59:08 +0000

    am

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-tamasgavlider-restapi-sp7zn655y4z.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-tamasgavlider-restapi-sp7zn655y4z.ws.codeinstitute-ide.net', os.getenv('ALLOWED_HOST')]
 
 
 # Application definition
