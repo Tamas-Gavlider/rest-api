4e951da2422b691c25576e0f2ee2b550bb094cd0
Tamas Gavlider <6mv8g9c8@students.codeinstitute.net>
2024-12-19 13:17:00 +0000

    m

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -110,8 +110,9 @@ MIDDLEWARE = [
 if "CLIENT_ORIGIN" in os.environ:
     CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
 
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://.*\.codeinstitute-ide\.net$"
+else:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        r"^https://.*\.codeinstitute-ide\.net$"
 ]
 
 
